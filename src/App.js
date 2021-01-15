306ae0554cc6d7011652a6c14192a5839e6d0a9e
afif ahmed <afifahmed456123@gmail.com>
2021-01-15 21:39:25 +0530

    commit for docker build

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className='App'>
       <header className='App-header'>
         <img src={logo} className='App-logo' alt='logo' />
-        <p>I was changed on the feature branch.</p>
+        <p>I was changed on the main branch.</p>
         <a
           className='App-link'
           href='https://reactjs.org'
